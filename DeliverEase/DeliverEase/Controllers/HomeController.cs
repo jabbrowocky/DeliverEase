bfcf0d25862a3a8dfd13dea1a8f3488278dd9e0b
Jabbrowocky <mindbull3tz@gmail.com>
2018-01-12 09:31:01 -0600

    added admin page and corresponding redirection

--- DeliverEase/DeliverEase/Controllers/HomeController.cs
+++ DeliverEase/DeliverEase/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using DeliverEase.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,7 +13,39 @@ namespace DeliverEase.Controllers
     {
         public ActionResult Index()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                
+
+                if (isAdminUser())
+                {
+                    return RedirectToAction("Index", "Users");
+                }
+                
+            }
             return View();
+            
+            
+        }
+        public Boolean isAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                ApplicationDbContext context = new ApplicationDbContext();
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
         }
 
         public ActionResult About()
---
+++ DeliverEase/DeliverEase/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using DeliverEase.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DeliverEase.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        [Authorize]
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        
+    }
+}
